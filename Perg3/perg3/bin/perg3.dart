ddabaf8900ab57215b9d7366aca26b2a0609bbc6
Filipe Ferreira <filipecbr@gmail.com>
2022-11-26 12:53:16 +0000

    Inicial

---
+++ Perg3/perg3/bin/perg3.dart
@@ -0,0 +1,5 @@
+import 'package:perg3/perg3.dart' as perg3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${perg3.calculate()}!');
+}
---
+++ Perg3/perg3/test/perg3_test.dart
@@ -0,0 +1,8 @@
+import 'package:perg3/perg3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
