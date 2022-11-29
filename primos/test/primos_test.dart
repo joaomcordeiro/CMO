da7f00976cf1d2e7f539f10f8eae40f7609be958
joaomcordeiro <61886873+joaomcordeiro@users.noreply.github.com>
2022-11-29 18:23:43 +0000

    Add files via upload

---
+++ primos/test/primos_test.dart
@@ -0,0 +1,8 @@
+import 'package:primos/primos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
---
+++ sortear/test/sortear_test.dart
@@ -0,0 +1,8 @@
+import 'package:sortear/sortear.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
