849959b65d5491a7544215357916f0678bf158e0
Filipe Ferreira <filipecbr@gmail.com>
2022-12-04 23:11:34 +0000

    Final com Perg2

--- Perg2/bin/sortear.dart
+++ Perg2/bin/sortear.dart
@@ -13,62 +13,69 @@ void main() {
   int maior = numeros[numeros.length - 1];
   var impares = [];
   var pares = [];
+  int terminar = 0;
+  do {
+    print("");
+    print("--------------------------------------------------------------");
+    print("                              MENU                            ");
+    print("--------------------------------------------------------------");
+    print("A - O maior e menor elemento do conjunto");
+    print("B - A amplitude do conjunto");
+    print("C - Uma listagem dos números ímpares com ordenação decrescente");
+    print("D- Visualização do conjunto");
+    print("E- Terminar");
+    print("--------------------------------------------------------------");
+    print("");
 
-  print("");
-  print("--------------------------------------------------------------");
-  print("                              MENU                            ");
-  print("--------------------------------------------------------------");
-  print("A - O maior e menor elemento do conjunto");
-  print("B - A amplitude do conjunto");
-  print("C - Uma listagem dos números ímpares com ordenação decrescente");
-  print("D- Visualização do conjunto");
-  print("--------------------------------------------------------------");
-  print("");
+    print("Selecione uma opção:");
+    var option = stdin.readLineSync();
 
-  print("Selecione uma opção:");
-  var option = stdin.readLineSync();
-
-  switch (option) {
-    case "A":
-      {
-        print(
-            'O menor número é ${numeros[0]}, o maior é ${numeros[numeros.length - 1]}');
-      }
-      break;
-
-    case "B":
-      {
-        print('A amplitude do conjunto é ${maior - menor}');
-      }
-      break;
-
-    case "C":
-      {
-        //distingue os numeros que sao pares dos que não são e coloca-os em listas separadas
-        for (final i in numeros) {
-          if (i.isEven) {
-            pares.add(i);
-          } else {
-            impares.add(i);
-          }
-          //ordena numeros impares por ordem descrescente
-          impares.sort((a, b) => b.compareTo(a));
+    switch (option) {
+      case "A":
+        {
+          print(
+              'O menor número é ${numeros[0]}, o maior é ${numeros[numeros.length - 1]}');
         }
+        break;
 
-        print('A lista de números impares por ordem decrescente é: $impares');
-      }
-      break;
+      case "B":
+        {
+          print('A amplitude do conjunto é ${maior - menor}');
+        }
+        break;
 
-    case "D":
-      {
-        print('O conjunto é composto por: $numeros');
-      }
-      break;
+      case "C":
+        {
+          //distingue os numeros que sao pares dos que não são e coloca-os em listas separadas
+          for (final i in numeros) {
+            if (i.isEven) {
+              pares.add(i);
+            } else {
+              impares.add(i);
+            }
+            //ordena numeros impares por ordem descrescente
+            impares.sort((a, b) => b.compareTo(a));
+          }
 
-    default:
-      {
-        print("Opção Inválida!");
-      }
-      break;
-  }
+          print('A lista de números impares por ordem decrescente é: $impares');
+        }
+        break;
+
+      case "D":
+        {
+          print('O conjunto é composto por: $numeros');
+        }
+        break;
+      case "E":
+        {
+          terminar = 1;
+        }
+        break;
+      default:
+        {
+          print("Opção Inválida!");
+        }
+        break;
+    }
+  } while (terminar == 0);
 }
--- Perg2/test/sortear_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:sortear/sortear.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
