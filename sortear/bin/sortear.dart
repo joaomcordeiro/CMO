2360b00a68157fa64a388b5c933765afb227e00a
joaomcordeiro <61886873+joaomcordeiro@users.noreply.github.com>
2022-12-04 16:39:35 +0000

    Add files via upload

--- sortear/bin/sortear.dart
+++ sortear/bin/sortear.dart
@@ -1,53 +1,67 @@
 import 'dart:io';
 import 'dart:math';
-import 'package:string_scanner/string_scanner.dart';
 
 void main() {
-  Random rnd;
-  int min = 90;
-  int max = 150;
-  rnd = Random();
-  int r = min + rnd.nextInt(max - min);
-  var numeros = [r];
-  var primos = [];
-  String option = "A";
-  print("MENU");
+  //é gerado um numero entre 90 e 150
+  //de seguida, esse número é usado como quantidade de números gerados aleatoriamente que populam a lista 'numeros'
+  //Depois, a lista é ordenade crescentemente
+  int r = Random().nextInt(60) + 90;
+  var numeros = List<int>.generate(r, (e) => Random().nextInt(100000));
+  numeros.shuffle();
+  numeros.sort();
+  int menor = numeros[0];
+  int maior = numeros[numeros.length - 1];
+  var impares = [];
+  var pares = [];
+
+  print("");
+  print("--------------------------------------------------------------");
+  print("                              MENU                            ");
+  print("--------------------------------------------------------------");
   print("A - O maior e menor elemento do conjunto");
   print("B - A amplitude do conjunto");
   print("C - Uma listagem dos números ímpares com ordenação decrescente");
   print("D- Visualização do conjunto");
+  print("--------------------------------------------------------------");
+  print("");
+
   print("Selecione uma opção:");
+  var option = stdin.readLineSync();
 
   switch (option) {
     case "A":
       {
-        numeros.sort();
         print(
-            'O maior número é ${numeros[0]}, o menor é ${numeros[numeros.length - 1]}');
+            'O menor número é ${numeros[0]}, o maior é ${numeros[numeros.length - 1]}');
       }
       break;
 
     case "B":
       {
-        print(numeros.length - 1);
+        print('A amplitude do conjunto é ${maior - menor}');
       }
       break;
 
     case "C":
       {
-        for (var i = 0; i <= r; i++) {
-          if (isPrime(numeros[i])) {
-            primos.add(i);
-          } else {}
+        //verifica os numeros que sao pares e os que sao impares e coloca numa lista
+        for (final i in numeros) {
+          if (i.isEven) {
+            pares.add(i);
+          } else {
+            impares.add(i);
+          }
+          //ordena numeros impares por ordem descrescente
+          impares.sort((a, b) => b.compareTo(a));
         }
-        primos.sort((b, a) => a.compareTo(b));
-        print(primos);
+
+        print('A lista de números impares por ordem decrescente é: $impares');
       }
       break;
 
     case "D":
       {
-        print(numeros);
+        print('O conjunto é composto por: $numeros');
       }
       break;
 
